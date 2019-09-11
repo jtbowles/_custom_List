e59dafac39a84e2c028a0f0a31b673561a4c8e04
Jason Bowles <jbowles96@gmail.com>
2019-09-11 12:50:42 -0500

    RemoveTest.cs initialized

---
+++ CustomListTest/RemoveTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyCustomList;
+
+namespace CustomListTest
+{
+    [TestClass]
+    public class RemoveTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // arrange
+
+            // act
+
+            // assert
+        }
+    }
+}
